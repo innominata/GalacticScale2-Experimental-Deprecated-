6fbbe35d95265f015d32e9e82aafdc3760f42686
innominata <inno@xray.cat>
2022-02-04 17:32:05 +1300

    GS2

---
+++ obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace GalacticScale3
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "GalacticScale";
+        public const string PLUGIN_NAME = "GalacticScale";
+        public const string PLUGIN_VERSION = "3.0";
+    }
+}
