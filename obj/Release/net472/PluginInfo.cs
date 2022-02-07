0e166dbdcbb3cf2a3b18083fbdff9b7398ed5f98
innominata <inno@xray.cat>
2022-02-08 01:22:27 +1300

    Delete obj directory

--- obj/Release/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace GalacticScale2
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "GalacticScale";
-        public const string PLUGIN_NAME = "GalacticScale";
-        public const string PLUGIN_VERSION = "2.3";
-    }
-}
